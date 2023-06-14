7a1ce7e48a0e2fe42c216a66a7a64fc9666dbcb8
AkshayRai <rai.axay@gmail.com>
2023-06-14 12:44:24 +0530

    optimized

--- adder_tb.sv
+++ adder_tb.sv
@@ -1,13 +1,13 @@
-module adder_tb(a,b,c,sum,cout);
-input logic [7:0] a;
-input logic [7:0] b;
-input logic c;
-output logic [7:0] sum;
-output logic cout;
+module adder_tb();
+logic [7:0] a;
+logic [7:0] b;
+logic c;
+logic [7:0] sum;
+logic cout;
 
 logic [8:0] result;
 
-adder ad1 (a,b,c,sum,cout);
+adder ad1 (.a(a),.b(b),.c(c),.sum(sum),.cout(cout));
 initial begin
 #50; 
 a=8'b00000101;b=8'b10010010;c=1'b0;
